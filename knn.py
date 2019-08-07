6fde8bfca732a560be852384c2d15dc5d9210ca4
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-08-07 17:23:47 -0400

    m

--- knn.py
+++ knn.py
@@ -1,5 +1,11 @@
 # knn machine learning, friend recommendation
 
+from surprise import KNNBasic
+import pandas as pd
+from surprise import Dataset
+from surprise import Reader
+
 movies = ['Star wars', 'Star wars', 'GOT', 'GOT', 'South Park', 'South Park', "Harry potter", "Harry potter"]
 rating = [1, 5, 1, 1, 5, 3, 2, 5]
 users = ['Kim', 'Tim', 'John', 'Jimmy', 'Julia', 'Kim', 'Jimmy', 'Kim']
+ 
