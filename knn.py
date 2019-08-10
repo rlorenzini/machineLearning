85582e145e775a162ead5226b8904546e932f808
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-08-09 21:48:19 -0400

    knn kim

--- knn.py
+++ knn.py
@@ -8,4 +8,30 @@ from surprise import Reader
 movies = ['Star wars', 'Star wars', 'GOT', 'GOT', 'South Park', 'South Park', "Harry potter", "Harry potter"]
 rating = [1, 5, 1, 1, 5, 3, 2, 5]
 users = ['Kim', 'Tim', 'John', 'Jimmy', 'Julia', 'Kim', 'Jimmy', 'Kim']
- 
+
+rating_dict = {
+'user': users,
+'item': movies,
+'rating': rating
+}
+
+def FriendRecommender(user):
+    df = pd.DataFrame(rating_dict)
+    reader = Reader(rating_scale=(1,5))
+    data = Dataset.load_from_df(df[['user', 'item', 'rating']],reader)
+    trainset = data.build_full_trainset()
+    sim_options = {
+        'name': 'cosine',
+        'user_based': True
+    }
+
+    algo = KNNBasic(sim_options)
+    algo.fit(trainset)
+
+    uid = trainset.to_inner_uid(user)
+    pred = algo.get_neighbors(uid, 3)
+
+    for i in pred:
+        print(trainset.to_raw_uid(i))
+
+FriendRecommender('Kim')
